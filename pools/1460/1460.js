0c8eeab0149cdf7f2199fced35762799308af7e3
jhsgy4f6b7-max <jhsgy4f6b7@privaterelay.appleid.com>
2026-01-02 16:44:53 +0000

    Add files via upload

---
+++ pools/1460/1460.js
@@ -0,0 +1,17 @@
+window.CPPool1460Init = function() { cp.model['1460Data']={
+};cp.poolSlideResources['pool1460']={
+};
+cp.model.data['1460']={
+pqs:[],
+gqs:[],
+sqs:[],
+rgqs:[],
+rsqs:[],
+hasCC:false
+};
+cp.poolResources["1460Images"]=[];
+cp.poolResources["1460Videos"]=[
+];
+cp.poolResources["1460SlideVideos"]=[
+];
+}
